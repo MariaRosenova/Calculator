1e08a0db0eed117d1eb88e0edc61a3d48b2ba98f
mims_3 <93831078+MariaRosenova@users.noreply.github.com>
2023-07-05 19:37:23 +0300

    Updated CSS

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ function App() {
   return (
     <div className="calculator-grid">
       <div className="output">
-        <div className="previous-operand">100000000000000000000000000000000</div>
-        <div className="current-operand">20</div>
+        <div className="previous-operand"></div>
+        <div className="current-operand"></div>
       </div>
       <button className="span-two">AC</button>
      <button>DEL</button>
