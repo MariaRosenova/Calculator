1514fd64152f3e6eb017f5b4a97d69042c2b094d
Maria <mimito1353@gmail.com>
2023-08-14 18:42:20 +0300

    Add functionality to DELETE button.

--- src/App.js
+++ src/App.js
@@ -19,13 +19,13 @@ function reducer(state, { type, payload }) {
           ...state,
           currentOperand: payload.digit,
           overwrite: false,
-        }
+        };
       }
       if (payload.digit === "0" && state.currentOperand === "0") return state;
       if (payload.digit === "." && state.currentOperand.includes(".")) {
         return state;
       }
-      
+
       return {
         ...state,
         currentOperand: `${state.currentOperand || ""} ${payload.digit}`,
@@ -61,21 +61,43 @@ function reducer(state, { type, payload }) {
 
     case ACTIONS.CLEAR:
       return {};
+    case ACTIONS.DELETE_DIGIT:
+      if (state.overwrite) {
+        return {
+          ...state,
+          overwrite: false,
+          currentOperand: null,
+        };
+      }
+      if (state.currentOperand == null) return state;
+      if (state.currentOperand.length === 1) {
+        return {
+          ...state,
+          currentOperand: null,
+        };
+      }
 
+      return {
+        ...state,
+        currentOperand: state.currentOperand.slice(0, -1),
+      };
+
+    // eslint-disable-next-line no-fallthrough
     case ACTIONS.EVALUATE:
-      if (state.operation == null || 
-          state.currentOperand == null ||
-          state.previousOperand == null) {
-            return state;
-          }
+      if (
+        state.operation == null ||
+        state.currentOperand == null ||
+        state.previousOperand == null
+      ) {
+        return state;
+      }
       return {
         ...state,
         overwrite: true,
         previousOperand: null,
         operation: null,
         currentOperand: evaluate(state),
-      }
-    
+      };
 
     default:
   }
@@ -126,7 +148,9 @@ function App() {
       >
         AC
       </button>
-      <button>DEL</button>
+      <button onClick={() => dispatch({ type: ACTIONS.DELETE_DIGIT })}>
+        DEL
+      </button>
       <OperationButton operation="÷" dispatch={dispatch} />
 
       <DigitButton digit="1" dispatch={dispatch} />
