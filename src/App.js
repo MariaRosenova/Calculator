782104e197c03b740a5bfb647f55d11aaba75fe9
Maria <mimito1353@gmail.com>
2023-08-14 09:04:55 +0300

    Add useReducer and implement the basic of the calculator logic.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,44 @@
+import { useReducer } from 'react';
+import DigitButton from './DigitButton.js';
 import "./styles.css";
 
+export const ACTIONS = {
+  ADD_DIGIT: "add-digit",
+  CHOOSE_OPERATION: "choose-operation",
+  CLEAR: "clear",
+  DELETE_DIGIT: "delete-digit",
+  EVALUATE: "evaluate",
+};
+
+const initialCalculatorState = {
+  currentOperand: "",
+  previousOperand: "",
+  operation: "",
+};
+
+function reducer(state, {type, payload}) {
+  switch(type) {
+    case ACTIONS.ADD_DIGIT:
+    return {
+      ...state,
+      currentOperand: `${state.currentOperand || ""} ${payload.digit}`
+    } 
+    default:
+  }
+};
+
 function App() {
+  const [{currentOperand, previousOperand, operation}, dispatch] = useReducer(reducer, initialCalculatorState )
+
   return (
     <div className="calculator-grid">
       <div className="output">
-        <div className="previous-operand"></div>
-        <div className="current-operand"></div>
+        <div className="previous-operand">{previousOperand} {operation}</div>
+        <div className="current-operand">{currentOperand}</div>
       </div>
       <button className="span-two">AC</button>
      <button>DEL</button>
-     <button>÷</button>
+     <DigitButton digit="÷" dispatch={dispatch}/>
      <button>1</button>
      <button>2</button>
      <button>3</button>
@@ -17,7 +46,7 @@ function App() {
      <button>4</button>
      <button>5</button>
      <button>6</button>
-     <button>+</button>
+     <button>÷</button>
      <button>7</button>
      <button>8</button>
      <button>9</button>
@@ -27,6 +56,6 @@ function App() {
      <button className="span-two">=</button>
     </div>
   );
-}
+};
 
 export default App;
